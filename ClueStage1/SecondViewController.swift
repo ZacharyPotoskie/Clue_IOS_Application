76c01dbc4a044caa590db768a105b308ddb2f89d
Holly Pemberton <hollypemberton@hollys-macbook-air.local>
2018-08-28 17:53:48 -0400

    Initial Commit

---
+++ ClueStage1/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ClueStage1
+//
+//  Created by Holly Pemberton on 8/28/18.
+//  Copyright © 2018 Zack. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
